a6d90c3053bd54754bdbe63149014bbd42145418
Binh Nguyen Van <binhnv@local.com>
2021-04-13 23:00:59 +0700

    fix: Update config

--- themes/set_theme.sh
+++ themes/set_theme.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 sudo docker cp vworkspace-theme nextcloud:/var/www/html/themes/
 sudo docker exec -uwww-data -it nextcloud php /var/www/html/occ config:system:set theme --value="vworkspace-theme"
+sudo docker exec -uwww-data -it nextcloud php /var/www/html/occ config:system:set updatechecker --value="true"
+sudo docker exec -uwww-data -it nextcloud php /var/www/html/occ config:system:set knowledgebaseenabled --value="false" --type=boolean
